a7a8c46d5cee680adc965defaf3770651de1aae8
Edwin <0701490@my.scccd.edu>
2025-12-03 19:34:15 +0000

    Solved for a b and c in the equations. Graphed the parabola and the three points set the x from -10 to 10. Set an output that prints a statement with the equation for said parabola.

--- SystemOfEquations3.1.m
+++ SystemOfEquations3.1.m
@@ -1,10 +1,5 @@
 %Create a script that will:
 
-%• Display the equation for that parabola on the screen
-%• Graph the parabola AND the three points that the user entered on the same plot for values of x from -10 to
-%10.
-% Include a title and grid for the plot
-
 %• Ask the user for three sets of (x,y) coordinates that a parabola passes
 %through.
 cord1 = input('Please enter the first set of coordinates as an array []:\n');
@@ -23,4 +18,20 @@ A = [x1^2 x1 1;
     x2^2 x2 1;
     x3^2 x3 1];
 B = [y1;y2;y3];
+
+% Solve for a b & c
+coeff = A\B;
+a = coeff(1);
+b = coeff(2);
+c = coeff(3);
+
+%• Display the equation for that parabola on the screen
+fprintf('\nThe Parabola Equation through those 3 points is:\n')
+fprintf('y = %.3fx^2 + %.3fx + %.3f\n',a,b,c)
+
+%• Graph the parabola AND the three points that the user entered on the same 
+% plot for values of x from -10 to 10. Include a title and grid for the plot
+
+x = -10:0.1:10;
+y = a*x.^2 + b*x + c;
 
