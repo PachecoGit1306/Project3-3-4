04b3e62c88f411d4762638a88551c3197b19d737
PachecoGit1306 <0933849@my.scccd.edu>
2025-12-03 16:39:43 -0800

    batting average and on base percentage

--- player.m
+++ player.m
@@ -14,10 +14,29 @@ classdef player
 
     methods
 
-        function obj = player(name, team, hits, walks, atbats)
+function obj = player(name, team, hits, walks, atbats)
             obj.name = name;
             obj.team = team;
             obj.hits = hits;
             obj.walks = walks;
             obj.atbats = atbats;
+        end
+
+        % Calculate batting average
+        function avg = get.battingAvg(obj)
+            if obj.atbats == 0
+                avg = 0;
+            else
+                avg = obj.hits / obj.atbats;
+            end
+        end
+
+        % Calculate on-base percentage
+        function pct = get.onBasePct(obj)
+            denominator = obj.atbats + obj.walks;
+            if denominator == 0
+                pct = 0;
+            else
+                pct = (obj.hits + obj.walks) / denominator;
+            end
         end
