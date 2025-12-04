79f4dde9407acdeaa913a65440ee9ea442734c9b
PachecoGit1306 <0933849@my.scccd.edu>
2025-12-03 16:41:24 -0800

    for loop for number of players

--- OOP_BaseballStatistics_3_4.m
+++ OOP_BaseballStatistics_3_4.m
@@ -1,1 +1,13 @@
+numPlayers = input("How many players do you want to enter:\n");
 
+players = player.empty;
+
+for i = 1:numPlayers
+    name = input("Enter the name of the baseball player: ", 's');
+    team = input("Enter the name of the team they play on: ", 's');
+    hits = input("Enter their number of hits: ");
+    atbats = input("Enter their number of at bats: ");
+    walks = input("Enter their number of walks: ");
+
+    players(i) = player(name, team, hits, walks, atbats);
+end
